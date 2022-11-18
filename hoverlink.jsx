fe42bb9530152afbe0e3974b95d013525d440bd3
Daniel Huebschmann <105037901+danielhdev@users.noreply.github.com>
2022-11-18 10:06:52 +0100

    Update hoverlink.jsx

--- hoverlink.jsx
+++ hoverlink.jsx
@@ -1,5 +1,5 @@
 <p className="relative w-fit one">
-        <span>Hello World! </span>
+        <h1>Hello World! </h1>
         <span className="absolute -bottom-1 left-0 w-0 transition-all h-1 bg-slate-400"></span>
         <span className="absolute top-1 right-0 w-0 transition-all h-1 bg-slate-400"></span>
       </p>
